8bbd5e7a98c05cfd5b811cb1bea5b06fd35a363c
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-10-31 11:49:00 -0700

    7:48 PM

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 for i in range(122, 96, -1):
     print("{:c}{:c}".format(i, i - 33), end='')
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    new_string = ""
-    for i in range(len(str)):
-        if i != n:
-            new_string += str[i]
-    return new_string
+    if n < 0:
+        return str
+    return str[:n] + str[n+1:]
