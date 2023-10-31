fac6d32259dbdedfaa220477410ac67a23a8f991
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-10-31 11:41:55 -0700

    7:41 PM

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 def remove_char_at(str, n):
-    if n < 0:
-        return str
-    return str[:n] + str[n+1:]
+    new_string = ""
+    for i in range(len(str)):
+        if i != n:
+            new_string += str[i]
+    return new_string
