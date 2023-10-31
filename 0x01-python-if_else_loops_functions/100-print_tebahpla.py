291e4a415aa743d29400b2fdf74a605e9f7ae4b2
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-10-31 11:52:39 -0700

    7:52 PM

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(122, 96, -1):
-    print("{:c}{:c}".format(i, i - 33), end='')
+for i in range(122, 96, -2):
+    print("{:c}{:c}".format(i, i - 1 - 32), end='')
