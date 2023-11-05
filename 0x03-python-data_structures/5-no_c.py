d53d62b8403fa232525bcd980575ff777b151d76
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-11-05 01:37:21 -0800

    Correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = "".join(char for char in my_string
-                         if char != 'c' and char != 'C')
+    new_string = ""
+
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+
     return new_string
