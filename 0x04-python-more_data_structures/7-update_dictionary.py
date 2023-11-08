af13a4f1dfa01b1ccd1dec5d040c52327d9c0a75
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-11-08 03:17:53 -0800

    Task

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,8 +6,9 @@ def update_dictionary(a_dictionary, key, value):
 
 if __name__ == "__main__":
     update_dictionary = __import__('7-update_dictionary').update_dictionary
-    print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
+    print_sorted_dictionary = (
+        __import__('6-print_sorted_dictionary').print_sorted_dictionary
+    )
     a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
     print_sorted_dictionary(new_dict)
