e5ddc91aa6698cf92e5192a8114d2042886ec98b
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-12-11 02:29:09 -0800

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
+
     for i in range(list_length):
         try:
             element_1 = my_list_1[i] if i < len(my_list_1) else 0
