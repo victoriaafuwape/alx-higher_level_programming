34853ba103f3af5ad4bba725c2f040d1b21fb1b9
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-12-20 23:50:56 -0800

    Task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             value = my_list[i]
             if isinstance(value, int):
                 print("{:d}".format(value), end="")
                 count += 1
-        print()
-        return count
-    except IndexError:
-        print()
-        return count
+        except IndexError:
+            raise
+    print()
+    return count
