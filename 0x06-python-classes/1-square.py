b9ae82b347dffb8b307f3c85ac08f85a0630897e
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-12-21 04:07:43 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module 1-square with class Square that defines a square."""
+
+
+class Square:
+    """A class Square that defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
