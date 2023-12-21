ac7be85f0e0ca899b46365125bc158bcacb78281
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-12-21 04:15:53 -0800

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""Module 4-square with class Square that defines a square."""
+
+
+class Square:
+    """A class Square that defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
