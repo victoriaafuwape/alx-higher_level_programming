1a34d18584139343c54330fbfd1f8bb70e508b42
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-12-21 01:19:44 -0800

    Task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -6,10 +6,31 @@
  */
 void print_python_list(PyObject *p)
 {
+	Py_ssize_t size, allocated, i;
+	PyObject *item;
+
 	if (!PyList_Check(p))
 	{
 		printf("[ERROR] Invalid List Object\n");
 		return;
+	}
+
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %zd: ", i);
+		if (PyBytes_Check(item))
+		{
+			printf("bytes\n");
+			print_python_bytes(item);
+		}
 	}
 }
 
