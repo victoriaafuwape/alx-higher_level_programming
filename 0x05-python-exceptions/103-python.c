0438b520adfe3fcd2d16e2bf269bd4633dca1e78
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-12-21 00:44:58 -0800

    Task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -20,7 +20,7 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	Py_buffer view;
-	int i;
+	int bytes_to_print, i;
 
 	if (!PyBytes_Check(p))
 	{
@@ -37,11 +37,12 @@ void print_python_bytes(PyObject *p)
 	printf("  size: %zd\n", view.len);
 	printf("  trying string: %.*s\n", (int)view.len, (char *)view.buf);
 	printf("  first 10 bytes: ");
-	for (i = 0; i < view.len && i < 10; ++i)
+	bytes_to_print = view.len < 10 ? view.len : 10;
+	for (i = 0; i < bytes_to_print; ++i)
 	{
 		printf("%02x ", (unsigned char)(((char *)view.buf)[i]));
 	}
-	if (view.len > 10)
+	if (view.len < 10)
 	{
 		printf("00");
 	}
