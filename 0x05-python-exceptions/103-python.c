0a6e00e2fe2bee99d426fe538eabf4307147dda3
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-12-21 02:15:35 -0800

    Task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -40,6 +40,7 @@ void print_python_list(PyObject *p)
 
     if (!PyList_Check(p))
     {
+	printf("[*] Python list info\n");
         printf("[ERROR] Invalid List Object\n");
         return;
     }
