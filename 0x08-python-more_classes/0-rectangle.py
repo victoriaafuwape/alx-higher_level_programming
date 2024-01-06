5b58ce7c22ec830e44fcea01abfe69e8ee60897a
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-01-06 00:23:48 -0800

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module defines an empty class Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty class Rectangle that defines a rectangle.
+    """
+    pass
