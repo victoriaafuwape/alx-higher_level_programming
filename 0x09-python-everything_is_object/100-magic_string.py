7640b43060c671ac21270af840dd599d818a9b16
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-01-06 02:05:04 -0800

    Task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def magic_string():
-    magic_string.count = getattr(magic_string, 'count', -1) + 1
-    return "BestSchool" + ", BestSchool" * magic_string.count
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return "BestSchool" + ", BestSchool" * (magic_string.counter - 1)
