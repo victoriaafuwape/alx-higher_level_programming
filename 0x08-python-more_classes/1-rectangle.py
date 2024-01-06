eaeba5ec3a34db3a4c5e842416d10db1c299c214
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-01-06 00:31:44 -0800

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class Rectangle with private instance attributes
+(width and height) and properties for retrieving and setting them with
+appropriate error handling.
+"""
+
+
+class Rectangle:
+    """
+    A class that defines a rectangle with properties to retrieve and
+    set its width and height.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
