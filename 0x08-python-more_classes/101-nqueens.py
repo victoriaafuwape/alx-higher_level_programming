e15e9be283df4c6fda6e80213bac1e527dfd3fbd
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-01-06 01:06:51 -0800

    Task 10

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        solution.append([i, board[i]])
+    print(solution)
+
+
+def is_safe(board, row, col):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(n, board, row):
+    if row == n:
+        print_solution(board)
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(n, board, row + 1)
+
+
+def nqueens(n):
+    board = [-1] * n
+    solve_nqueens(n, board, 0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(n)
