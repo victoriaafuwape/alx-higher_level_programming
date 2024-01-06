2fe517480eaac70ac21ddac68d1dc79d2809e7c3
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-01-06 03:02:11 -0800

    Task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    A class that only allows creating an instance attribute named 'first_name'.
+    """
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError("'LockedClass' object has no attribute '"
+                                 + name + "'")
+        self.__dict__[name] = value
