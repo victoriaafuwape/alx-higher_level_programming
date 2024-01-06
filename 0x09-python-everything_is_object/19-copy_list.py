1d19942e39bcd5fe92b737b1f540684efdbf37f7
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-01-06 01:40:18 -0800

    Task 19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-"""
-Module for copying a list.
-"""
-
-
 def copy_list(original_list):
-    """
-    Create and return a copy of a list.
-    """
     return original_list.copy()
