0199bd84b3b71e4aebcf59380a6588c7fe003801
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-01-06 00:38:22 -0800

    Task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+This module further enhances the Rectangle class to include a string
+representation of the rectangle using '#' characters.
+"""
+
+class Rectangle:
+    """
+    A class that defines a rectangle with properties to retrieve and set its
+    width and height, methods to calculate its area and perimeter,
+    and a string representation method.
+    """
+
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return '\n'.join(['#' * self.__width for _ in range(self.__height)])
